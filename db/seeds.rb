b3109c39d60658b996bc169b9b050b8fdd0e0775
adamteddychang <adamchang0725@gmail.com>
2022-03-30 16:04:30 +0200

    completed api setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create(content: 'Message 1')
+Message.create(content: 'Message 2')
+Message.create(content: 'Message 3')
+Message.create(content: 'Message 4')
+Message.create(content: 'Message 5')
